a946261e984710e81c6147a9c741355547626a18
David Koncsol <davidkoncsol@Davids-MacBook-Pro.local>
2019-07-07 16:47:54 -0700

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,9 +1,19 @@
-var cardOne = "queen";
-var cardTwo = "king";
-var cardThree = "queen";
-var cardFour = "king";
+
+var cards =["queen", "queen", "king", "king"];
+
+var cardsInPlay = [];
+var cardOne =cards[0];
+cardsInPlay.push(cardOne);
 
 console.log("User flipped " + cardOne);
+
+var cardTwo = cards[2];
+cardsInPlay.push(cardTwo);
+
 console.log("User flipped " + cardTwo);
-console.log("User flipped " + cardThree);
-console.log("User flipped " + cardFour);
+
+if (cardsInPlay.length === 2 && cardOne === cardTwo){
+	alert("You found a match!");
+}else {
+	alert("Sorry, try again");
+}
